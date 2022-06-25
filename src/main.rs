39a42579b2268e2205d6957c718c61ae390c97bd
crywolfe <gwolfe2007@gmail.com>
2022-06-25 14:40:24 -0500

    support setup

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ extern crate sdl2;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{LoadTexture, INI&&T_PNG, INIT_JPG};
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::render::TextureCreator;
 use sdl2::render::Texture;
 use sdl2::video::{Window, WindowContext};
+use std::fs::File;
+use std::io::{self, Write, Read};
 use std::time::Duration;
 use std::thread::sleep;
 use std::time::SystemTime;
@@ -20,9 +23,51 @@ enum TextureColor {
     Blue
 }
 
+fn read_from_file(file_name: &str) -> io::Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn write_into_file(content: &str, file_name: &str) -> io::Result<()> {
+    let mut f = File::create(file_name)?;
+    f.write_all(content.as_bytes())
+}
+
+fn slice_to_string(slice: &[u32]) -> String {
+    slice.iter().map(|highscore| highscore.to_string()).collect::<Vec<String>>().join(" ")
+}
+
+fn save_highscores_and_lines(highscores: &[u32], number_of_lines: &[u32]) -> bool {
+
+    let s_highscores = slice_to_string(highscores);
+    let s_number_of_lines = slice_to_string(number_of_lines);
+    write_into_file(format!("{}\n{}\n", s_highscores, s_number_of_lines), "scores.txt").is_ok()
+}
+
+fn line_to_slice(line: &str) -> Vec<u32> {
+    line.split(" ").filter_map(|nb|nb.parse::<u32>().ok()).collect()
+}
+
+fn load_highscores_and_lines() -> Option<(Vec<u32>, Vec<u32>)> {
+    if let Ok(content) = read_from_file("scores.txt") {
+        let mut lines = content.splitn(2, "\n").map(|line| line_to_slice(line)).collect::<Vec<_>>();
+        if lines.len() == 2 {
+            let (number_lines, highscores) = (lines.pop().unwrap(), lines.pop().unwrap());
+            Some((highscores, number_lines))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
     let video_subsystem = sdl_context.video().expect("Couldn't get SLD video subsystem");
+
+    sdl2::image::init(INIT_PNG | INIT_JPG).expect("Couldn't initialize image context");
 
     // Parameters are title, width, height
     let window = video_subsystem.window("Tetris", 800, 600)
@@ -56,7 +101,7 @@ pub fn main() {
                 _ => {}
             }
         }
-        canvas.set_draw_color(Color::RGB(255,0,0));
+        canvas.set_draw_color(Color::RGB(0,0,0));
         canvas.clear();
 
         // rectangle switch
