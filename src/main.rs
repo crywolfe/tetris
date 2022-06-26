406d9c5558b8b6930802a238ce3df64b46e82882
crywolfe <gwolfe2007@gmail.com>
2022-06-25 20:44:33 -0500

    add initial logic.

--- src/main.rs
+++ src/main.rs
@@ -180,6 +180,32 @@ impl TetriminoGenerator for TetriminoT {
     }
 }
 
+impl Tetrimino {
+    fn rotate(&mut self) {
+        self.current_state += 1;
+        if self.current_state as usize >= self.states.len() {
+            self.current_state = 0;
+        }
+    }
+
+    fn test_position(&self, game_map: &[Vec<u8>], tmp_state: usize, x: isize, y: usize) -> {
+        for decal_y in 0..4 {
+            for decal_x in 0..4 {
+                let x = x + decal_x;
+                if self.states[tmp_state][decal_y][decal_x as usize] != 0 &&
+                    (y + decal_y >= game_map.len() ||
+                        x < 0 ||
+                        x as usize >= game_map[y+decal_y].len() ||
+                        game_map[y+decal_y][x as usize] != 0) {
+                            return false;
+                        }
+
+            }
+        }
+        return true;
+    }
+}
+
 fn read_from_file(file_name: &str) -> io::Result<String> {
     let mut f = File::open(file_name)?;
     let mut content = String::new();
